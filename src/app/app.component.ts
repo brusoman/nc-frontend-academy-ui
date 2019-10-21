7ab5d234e97e426c5d6a81afc1bdfe8ac725e11e
GravisUltrasound <keshyin@yandex.ru>
2019-10-22 01:19:34 +0300

    styles package added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['../styles/app.component.less']
 })
 export class AppComponent {
   title = 'nfa';
