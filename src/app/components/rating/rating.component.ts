8ba3fb5a703c0d7ac7b8b777b90587b449ae95e2
Farhad74 <evgeniy_mgn_74@mail.ru>
2019-12-01 15:07:57 +0300

    task page is corrected

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -11,8 +11,21 @@ export class RatingComponent implements OnInit {
   @Input() curTaskRait: TaskModel;
 
   constructor() { }
+  i: number = 0;
+  attemt: number[] = [];
+  setAtt(): void {
+    while (this.i < 10) {
+      if (this.i < this.curTaskRait.attemt) {
+        this.attemt[this.i] = 1;
+      } else {
+        this.attemt[this.i] = 0;
+      }
+      this.i++;
+    }
+  }
 
   ngOnInit() {
+    this.setAtt();
   }
 
 }
